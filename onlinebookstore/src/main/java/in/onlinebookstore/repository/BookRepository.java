14441b1cafa4c6b093be283de688d708b361d10a
hrshuv0 <59235318+hrshuv0@users.noreply.github.com>
2020-12-16 13:23:28 +0600

    updated angular project with backend

--- onlinebookstore/src/main/java/in/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package in.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.onlinebookstore.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
