e1bccd7649f0a07b033e4a99ae3a291cd681da45
hrshuv0 <59235318+hrshuv0@users.noreply.github.com>
2020-12-16 13:24:48 +0600

    updated angular project with spring boot and completed the feature one.

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[];
+  constructor(private _bookSErvice: BookService) { }
+
+  ngOnInit(): void {
+    this.listBooks();
+  }
+
+  listBooks(){
+    this._bookSErvice.getBooks().subscribe(data => {
+      console.log(data);
+      this.books = data;
+    });
+  }
+
+}
