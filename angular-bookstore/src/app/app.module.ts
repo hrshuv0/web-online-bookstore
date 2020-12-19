b8a231e0d18adead8ac62b6ae332c6f67bbd3929
hrshuv0 <59235318+hrshuv0@users.noreply.github.com>
2020-12-19 21:24:13 +0600

    Completed feature 3 search by category.

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
 
   books: Book[];
-  constructor(private _bookSErvice: BookService) { }
+  currentCategoryId: number;
+
+
+
+  constructor(private _bookService: BookService,
+    private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listBooks();
+    this._activatedRoute.paramMap.subscribe(() => {
+      this.listBooks();
+    });
   }
 
   listBooks(){
-    this._bookSErvice.getBooks().subscribe(data => {
+
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId)
+    {
+      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+    }
+    else{
+      this.currentCategoryId = 1;
+    }
+
+
+
+
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(data => {
       console.log(data);
       this.books = data;
     });
